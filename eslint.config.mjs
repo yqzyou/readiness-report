6046973c91c4f8439cd715317efe5df760da6c3c
ykz <598005203@qq.com>
2026-09-09 09:26:21 +0800

    chore: honor underscore prefix in unused-vars lint rule

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -9,6 +9,14 @@ const compat = new FlatCompat({ baseDirectory: __dirname })
 
 const eslintConfig = [
   ...compat.extends('next/core-web-vitals', 'next/typescript'),
+  {
+    rules: {
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        { argsIgnorePattern: '^_', varsIgnorePattern: '^_' },
+      ],
+    },
+  },
   { ignores: ['node_modules/**', '.next/**', 'out/**', 'coverage/**', 'data/**'] },
 ]
 
